92769b2320955e4fffa44be4dd5c2dbb5410a99c
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-06-15 14:35:43 -0400

    Add files via upload

---
+++ make_playlist.py
@@ -0,0 +1,113 @@
+'''
+Extract playlist URLs
+(for further processing)
+
+'''
+###########################################################################
+#                       IMPORT STATEMENTS                                ##
+###########################################################################
+import requests, json, os 
+from bs4 import BeautifulSoup
+from pytube import YouTube
+
+###########################################################################
+#                       HELPER FUNCTIONS                                ##
+###########################################################################
+def scrapelinks(playlist):
+    #https://www.youtube.com/playlist?list=PL1v-PVIZFDsqbzPIsEPZPnvcgIQ8bNTKS
+    page=requests.get(playlist)
+    soup=BeautifulSoup(page.content, 'lxml')
+
+    g=soup.find_all('tr',class_='pl-video yt-uix-tile ')
+    entries=list()
+    links=list()
+    totaltime=0
+
+    for i in range(len(g)):
+        try:
+            h=str(g[i])
+            
+            # get titles
+            h1=h.find('data-title="')+len('data-title="')
+            h2=h[h1:].find('"')
+            title=h[h1:h1+h2]
+
+            # get links
+            h3=h.find('data-video-id="')+len('data-video-id="')
+            h4=h[h3:].find('"')
+            link=base+h[h3:h3+h4]
+
+            # get duration (in seconds)
+            h5=h.find('<div class="timestamp"><span aria-label="')
+            h6=h[h5:]
+            hsoup=BeautifulSoup(h6,'lxml')
+            htext=hsoup.text.replace('\n','').replace(' ','')
+            hmin=htext.split(':')
+            duration=int(hmin[0])*60+int(hmin[1])
+            totaltime=totaltime+duration
+
+            if link not in links:
+
+                # avoids duplicate links 
+                links.append(link)
+
+                entry={
+                    'title':title,
+                    'link':link,
+                    'duration':duration
+                    }
+                
+                entries.append(entry)
+
+        except:
+            print('error')
+
+    return entries, len(entries), totaltime
+
+###########################################################################
+##                          MAIN CODE BASE                               ##
+###########################################################################
+
+base='https://www.youtube.com/watch?v='
+
+playlists=list()
+entries=list()
+t=1
+totalnum=0
+totaltime=0
+
+playlist_name=input('what do you want to name this playlist (e.g. angry)?')
+
+while t>0:
+
+    try:
+
+        playlist=input('what is the playlist id?')
+        if playlist not in ['', 'n']:
+            playlist='https://www.youtube.com/playlist?list='+playlist
+            playlists.append(playlist)
+            entry, enum, nowtime=scrapelinks(playlist)
+            totalnum=totalnum+enum
+            totaltime=totaltime+nowtime 
+            entries=entries+entry
+
+        else:
+            break
+
+    except:
+
+        print('error') 
+
+os.mkdir(playlist_name)
+os.chdir(os.getcwd()+'/'+playlist_name)
+
+data={
+    'entrynum':totanum,
+    'total time':totaltime,
+    'playlist url':playlists,
+    'entries':entries,
+}
+
+jsonfile=open(playlist_name+'.json','w')
+json.dump(data,jsonfile)
+jsonfile.close()
